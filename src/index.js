01b3f6a1c3d6764d9fccd8262d3da61a49d080b9
KAYSILVHA <kaysilvha@gmail.com>
2023-09-18 14:29:42 -0300

    adicionando intermediario

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { listagemCarros, obterCarro } = require('./controladores/carros');
-
+const {listagemCarros, obterCarro} = require('./controladores/carros');
+const {intermediarioVerificarSenha} = require('./intermediarios');
 const app = express();
 
+
+app.use(intermediarioVerificarSenha);
 app.get('/carros', listagemCarros);
 app.get('/carros/:id', obterCarro);
-
 
 app.listen(3000,()=>{
   console.log("http://localhost:3000/")
